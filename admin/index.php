8016084e6e3cf1ff657ad14f24ef0e632cb53ba9
Baptiste Lepers <baptiste.lepers@gmail.com>
2026-01-14 16:26:36 +0100

    New revision

--- admin/index.php
+++ admin/index.php
@@ -17,7 +17,7 @@ ini_set('session.use_trans_sid', 0);
 ini_set('session.use_only_cookies', 1);
 $DEBUG = 0;
 
-$VERSION = '250116';
+$VERSION = '260114';
 
 /* 1/ Set up autoloading */
 class AutoLoader {
